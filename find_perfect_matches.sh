4da899e4884153ad728608c789244960ece75a67
BowerH <hbower6@gatech.edu>
2024-09-09 18:40:35 -0400

    first commit

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,1 +1,9 @@
-echo "oops!"
+queryfile="$1"
+subjectfile="$2"
+outfile="$3"
+
+blastn -query "$queryfile" -subject "$subjectfile" -outfmt "6 pident length qlen slen" | \
+awk '$1 == 100'> "$outputfile"
+perfect_match=$(wc -l < "outputfile")
+echo number of perfet matches: $perfect_match
+
